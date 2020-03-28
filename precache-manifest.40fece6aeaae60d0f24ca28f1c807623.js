6535f2a3381dfb28828059fac174dbeb8c4e5f84
tajbst <tajbist2@gmail.com"exitgit config --global user.name "Taj Bist"git config --global user.email tajbist2@gmail.comexitgit config --global user.name Taj>
2020-03-28 16:40:11 -0400

    Updates

---
+++ precache-manifest.40fece6aeaae60d0f24ca28f1c807623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b61c2e2619b26424388ede76cebbcf",
+    "url": "/corona-info/index.html"
+  },
+  {
+    "revision": "f0400f730f52be87a1a4",
+    "url": "/corona-info/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "34bad5512428f071d26b",
+    "url": "/corona-info/static/css/main.8db714bf.chunk.css"
+  },
+  {
+    "revision": "f0400f730f52be87a1a4",
+    "url": "/corona-info/static/js/2.82003b05.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/corona-info/static/js/2.82003b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34bad5512428f071d26b",
+    "url": "/corona-info/static/js/main.ff8609aa.chunk.js"
+  },
+  {
+    "revision": "583e763c7b9b5b7c0151",
+    "url": "/corona-info/static/js/runtime-main.4088d11c.js"
+  }
+]);
